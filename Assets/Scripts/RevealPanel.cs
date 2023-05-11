33b1eb14d6ec2a1973a52b3ed4996d00b8d9e5aa
Scottx125 <31070452+Scottx125@users.noreply.github.com>
2023-05-11 16:54:42 +0100

    V 0.1
    UI Elements necessary for editing the car created and working. Next step is to create the scriptable object holding the information for each part.

---
+++ Assets/Scripts/RevealPanel.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RevealPanel : MonoBehaviour
+{
+    public GameObject parent;
+    public GameObject hiddenPanel;
+
+    RectTransform parentRect;
+    RectTransform hiddenPanelRect;
+    float hiddenPanelHeight;
+    float parentHeight;
+    float parentHeightOffset;
+
+    void Awake(){
+        parentRect = parent.GetComponent<RectTransform>();
+        hiddenPanelRect = gameObject.GetComponent<RectTransform>();
+        parentHeight = parentRect.rect.height;
+        parentHeightOffset = parent.GetComponent<VerticalLayoutGroup>().spacing;
+        hiddenPanelHeight = hiddenPanelRect.rect.height;
+    }
+
+    public void OnButtonClick(){
+        if (gameObject.activeSelf == false){
+            SetActive();
+        } else {SetInactive();}
+    }
+
+    void SetActive(){
+        hiddenPanel.SetActive(true);
+        float combinedHeight = parentHeight + hiddenPanelHeight;
+        parentRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, combinedHeight + parentHeightOffset);
+    }
+    void SetInactive(){
+        hiddenPanel.SetActive(false);
+        parentRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, parentHeight);
+    }
+}
